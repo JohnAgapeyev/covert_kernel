da2c06294931dabdf8ee27a9527545fbb74542ab
John Agapeyev <jagapeyev@gmail.com>
2018-09-02 10:44:06 -0700

    Added main to make userspace a real application

--- userspace.c
+++ userspace.c
@@ -1,6 +1,11 @@
 #include <openssl/evp.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define TAG_LEN 16
+#define NONCE_LEN 12
+#define KEY_LEN 16
 
 unsigned char* encrypt_data(const unsigned char* message, const size_t mesg_len,
         const unsigned char* key, const unsigned char* nonce, const unsigned char* aad,
@@ -12,13 +17,13 @@ unsigned char* encrypt_data(const unsigned char* message, const size_t mesg_len,
     EVP_EncryptUpdate(ctx, NULL, &len, aad, aad_len);
 
     //Allocate enough for the message and the tag
-    unsigned char* ciphertext = malloc(mesg_len + 16);
+    unsigned char* ciphertext = malloc(mesg_len + TAG_LEN);
 
     EVP_EncryptUpdate(ctx, ciphertext, &len, message, mesg_len);
 
     EVP_EncryptFinal_ex(ctx, ciphertext + len, &len);
 
-    EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_GET_TAG, 16, ciphertext + len);
+    EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_GET_TAG, TAG_LEN, ciphertext + len);
 
     EVP_CIPHER_CTX_free(ctx);
 
@@ -34,15 +39,15 @@ unsigned char* decrypt_data(unsigned char* message, const size_t mesg_len, const
     int len;
     EVP_DecryptUpdate(ctx, NULL, &len, aad, aad_len);
 
-    if (mesg_len <= 16) {
+    if (mesg_len <= TAG_LEN) {
         return NULL;
     }
 
-    unsigned char* plaintext = malloc(mesg_len - 16);
+    unsigned char* plaintext = malloc(mesg_len - TAG_LEN);
 
-    EVP_DecryptUpdate(ctx, plaintext, &len, message, mesg_len - 16);
+    EVP_DecryptUpdate(ctx, plaintext, &len, message, mesg_len - TAG_LEN);
 
-    if (!EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_TAG, 16, message + mesg_len - 16)) {
+    if (!EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_TAG, TAG_LEN, message + mesg_len - TAG_LEN)) {
         return NULL;
     }
 
@@ -53,4 +58,30 @@ unsigned char* decrypt_data(unsigned char* message, const size_t mesg_len, const
     EVP_CIPHER_CTX_free(ctx);
 
     return plaintext;
+}
+
+int main(void) {
+    const char* m = "Hello World";
+
+    unsigned char nonce[NONCE_LEN];
+    memset(nonce, 0xab, NONCE_LEN);
+
+    unsigned char key[KEY_LEN];
+    memset(key, 0xfe, KEY_LEN);
+
+    const char* aad = "Goodbye World";
+
+    unsigned char* ciphertext = encrypt_data((const unsigned char*) m, strlen(m), key, nonce,
+            (const unsigned char*) aad, strlen(aad));
+
+    unsigned char* plaintext = decrypt_data(ciphertext, strlen(m), key, nonce,
+            (const unsigned char*) aad, strlen(aad));
+
+    if (memcmp(m, plaintext, strlen(m)) == 0) {
+        puts("Encryption works fine");
+    } else {
+        puts("Encryption FAILED");
+    }
+
+    return EXIT_SUCCESS;
 }
